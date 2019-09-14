663e24d3133863b0d022b1acebe5a6d7b61bb4ca
bichnln <bich.nln@gmail.com>
2019-09-14 17:17:54 +1000

    fixed addsales.js and addsales.php

---
+++ apps/view/addsales.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset='utf-8'/>
+		<meta name='author' content='DP2'/>
+		<title>PHP - Add Sales Records</title>
+		<script src="../../public/js/addsales.js"></script>
+	</head>
+	<body>
+		<?php 
+		include_once "header.inc";
+		?>
+		<!-- add php to process this form to db later -->
+		<form action="addsalestodb.php" method="post">
+			<fieldset>
+				<p>
+					<label for='ItemId'>Item ID</label>
+					<input id='ItemId' name='itemid' type='text' required='required' pattern="^[a-zA-Z]+$"/>
+				</p>
+				<p>
+					<label for='ItemName'>Item name</label>
+					<input id='ItemName' name='itemname' type='text' required='required' pattern="^[a-zA-Z ]+$"/>
+				</p>
+				<p>
+					<label for='Price'>Price</label>
+					<input id='Price' name='price' type='text' required='required' pattern="^([0-9])+(\.[0-9]{0,2})?$"/>
+				</p>
+				<p>
+					<label for='Quantity'>Quantity</label>
+					<input id='Quantity' name='quantity' type='text' required='required' pattern="\d+$"/>
+				</p>
+				<p>
+					<label for='Date'>Date Sold</label>
+					<input id='Date' name='date' type='date' required='required'/>
+				</p>
+				<p>
+				<span class="errortxt" id="statetxt"></span>
+				</p>
+				<p>
+    			<button id="addbtn" class="btn" type="submit" name="submit" value="addsales">Add</button>				
+				<input type="reset" value="Reset Form"/>
+				</p>
+			</fieldset>
+		</form>
+	</body>
+</html>
--- public/js/addsales.js
+++ public/js/addsales.js
@@ -1,47 +1,39 @@
+function onInputValidate(value, pattern, errorText) {
+    var statetxt = document.getElementById("statetxt"); 
+    if (!value.match(pattern)) {
+        statetxt.style.display = "block";
+        statetxt.style.color = "red";
+        statetxt.innerHTML = errorText;
 
+    } else {
+        statetxt.innerHTML = "";
+        statetxt.style.display = "none";
+    }
+}
 function init() {
     var itemID = document.getElementById('ItemId');
     var itemName = document.getElementById('ItemName');
-    var price = document.getElementById("Price");
-    var qty = document.getElementById("Quantity");
+    var price = document.getElementById('Price');
+    var qty = document.getElementById('Quantity');
     var date = document.getElementById("Date");
-    var statetxt = document.getElementById("statetxt");
-    var addSalesForm = document.getElementById("addSalesForm");
+    var statetxt = document.getElementById("statetxt"); 
+    var addSalesForm = document.getElementById("addSalesForm"); 
     
-    
-    var idPattern = /^[a-zA-Z]+$/;
-    var namePattern = /^[A-Za-z ]+/;
-    var numberPattern = /^[0-9]+]/;
-    itemName.onkeypress = function() {  
-        if (!itemName.value.match(namePattern)) {
-            statetxt.innerHTML = "Item name can contain only letters and whitespaces!";
-            statetxt.style.color = "red";
-            statetxt.style.display = "block";
-        } else {
-            statetxt.innerHTML = "";
-        }
+    var idPattern = /^([0-9]){1,11}$/;
+    var namePattern = /^[A-Za-z ]+$/;
+    var pricePattern = /^([0-9])+(\.[0-9]{0,2})?$/;
+    var qtyPattern = /^[0-9]+$/;
 
-    }
-    itemID.onkeypress = function() {
-        if (!itemID.value.match(idPattern)) {
-            statetxt.innerHTML = "Item id can contain only letters!";
-            statetxt.style.color = "red";
-            statetxt.style.display = "block";
-        } else {
-            statetxt.innerHTML = "";
-        }
-    }
-    price.onkeypress = function() {
-        if (!price.value.match(numberPattern)) {
-            statetxt.innerHTML = "Price can only has numerical value!";
-            statetxt.style.color = "red";
-            statetxt.style.display = "block";
-        } else {
-            statetxt.innerHTML = "";
-        }
-    }
-    qty.onselect = function() {
-        if (!qty.value.onkeypress(numberPattern)) {
+    itemID.oninput = onInputValidate(itemID.value, idPattern, "ID must not be empty and contains only numbers with 11 digits as max");
+
+    itemName.oninput = onInputValidate(itemName.value, namePattern,  "Item Name must not be empty and contains only letters and spaces");
+    
+    price.oninput = onInputValidate(price.value, pricePattern, "Price must not be empty and accepts only decimal numbers");
+
+    qty.oninput = onInputValidate(qty.value, qtyPattern, "Quantity must not be empty and accepts only positive integers");
+    qty.onkeypress = function() {
+        if (!qty.value.match(qtyPattern
+    )) {
             statetxt.innerHTML = "Quantity can only has numerical value!";
             statetxt.style.color = "red";
             statetxt.style.display = "block";
@@ -56,8 +48,7 @@ function init() {
             statetxt.style.display = "block";
         } else {
             statetxt.innerHTML = "";
-        }
-        
+        }       
     }
 }
 window.onload = init;
