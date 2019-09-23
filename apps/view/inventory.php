b8255992aed918e94a0b9e6bec7bcdb707dbb406
102101701@student.swin.edu.au <102101701@student.swin.edu.au>
2019-09-23 13:10:54 +1000

    addinventory

--- apps/view/inventory.php
+++ apps/view/inventory.php
@@ -1,33 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
-	<?php include 'header.inc';?>
+	<head>
+		<meta charset='utf-8'/>
+		<meta name='author' content='DP2'/>
+		<title>PHP - Add Inventory Records</title>
+	</head>
 	<body>
+	<?php include_once 'header.inc';?>
 		<h1>Add an Inventory Record</h1>
-		<form method="post" action="result.php">
+		<form method="post">
 			<fieldset>
 				<legend>Inventory Record</legend>
-				<table>
-					<tr>
-						<td>
-							<label>Product ID</label>
-							<input type="text" name="proID" id="ProID" maxlength="25" size="25" required="required" pattern="\d*"/>
-						</td>
-						<td>
-							<label>Product Name</label>
-							<input type="text" name="proName" id="ProName" maxlength="25" size="25" required="required"/>
-						</td>
-						<td>
-							<label>Product Price</label>
-							<input type="text" name="proPrice" id="ProPrice" maxlength="7" size="7" required="required" pattern="\d*?\.?\d{1,2}"/>
-						</td>
-						<td>
-							<label>Quantity in Stock</label>
-							<input type="text" name="proQty" id="ProQty" maxlength="3" size="3" required="required" pattern="\d*"/>
-						</td>
-					</tr>
-				</table>
+					<p>
+						<label for="itemID">Item ID</label>
+						<input type="text" name="itemID" id="itemID" maxlength="10" size="10" required="required" pattern="\d*"/>
+					</p>
+					<p>
+						<label for="itemName">Product Name</label>
+						<input type="text" name="itemName" id="itemName" maxlength="40" size="40" required="required"/>
+					</p>
+					<p>
+						<label for="itemPrice">Product Price</label>
+						<input type="text" name="itemPrice" id="itemPrice" maxlength="8" size="8" required="required" pattern="\d*?\.?\d{1,2}"/>
+					</p>
+					<p>
+						<label for="itemStock">Quantity in Stock</label>
+						<input type="text" name="itemStock" id="itemStock" maxlength="10" size="10" required="required" pattern="\d*"/>
+					</p>
+					<p>
+					<span class="errortxt" id="statetext"></span>
+					</p>
+				<p>
 				<input type="submit" value="Submit"/>
 				<input type="reset" value="Reset"/>
+				</p>
 			</fieldset>
 		</form>
 	</body>
