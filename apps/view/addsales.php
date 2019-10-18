1feee578e249cc6678599430aeec2f5562558512
102101701@student.swin.edu.au <102101701@student.swin.edu.au>
2019-10-19 09:34:31 +1100

    CSS and fixes to pages

--- apps/view/addsales.php
+++ apps/view/addsales.php
@@ -7,135 +7,138 @@
 		<script src="../../public/js/addsales.js"></script>
 	</head>
 	<body>
-		<?php 
-		include_once "header.inc";
-		?>
-		<!-- add search feature -->
-		<?php 
-			session_start();
-			if (isset($_SESSION['items'])) {
-				$data = $_SESSION['items'];
-				// echo $data;
-				if(is_array($data)) {
-		?>
-					<table border='1'>
-						<tr>
-							<th>Item Name</th>
-							<th>Item Price</td>
-							<th>Quantity in Stock</th>
-							<th>Category</th>
-						</tr>
-					<?php 
-					foreach($data as $row) {
-						echo "<tr>";
-						echo "<td>" . $row[1] . "</td>";
-						echo "<td>" . $row[2] . "</td>";
-						echo "<td>" . $row[3] . "</td>";
-						echo "<td>" . $row[4] . "</td>";
-						echo "</tr>";
+	<div class="content">
+			<?php 
+			include_once "header.inc";
+			?>
+			<?php include 'menu.inc';?>
+			<!-- add search feature -->
+			<?php 
+				session_start();
+				if (isset($_SESSION['items'])) {
+					$data = $_SESSION['items'];
+					// echo $data;
+					if(is_array($data)) {
+			?>
+						<table border='1'>
+							<tr>
+								<th>Item Name</th>
+								<th>Item Price</td>
+								<th>Quantity in Stock</th>
+								<th>Category</th>
+							</tr>
+						<?php 
+						foreach($data as $row) {
+							echo "<tr>";
+							echo "<td>" . $row[1] . "</td>";
+							echo "<td>" . $row[2] . "</td>";
+							echo "<td>" . $row[3] . "</td>";
+							echo "<td>" . $row[4] . "</td>";
+							echo "</tr>";
+						}
+
+						?>
+						
+						</table>
+			<?php
 					}
 
-					?>
-					
-					</table>
-		<?php
-				}
+					if (isset($_GET['com'])) {
+						if ($_GET['com'] != 1)
+							echo "Uh-oh! There is something wrong with the system.";
+					}	
 
-				if (isset($_GET['com'])) {
-					if ($_GET['com'] != 1)
-						echo "Uh-oh! There is something wrong with the system.";
-				}	
-
-			} else {
-				if (isset($_GET['com'])) {
-					if ($_GET['com'] == 2) {
-						echo "Uh-oh! There is no item in the inventory.";
+				} else {
+					if (isset($_GET['com'])) {
+						if ($_GET['com'] == 2) {
+							echo "Uh-oh! There is no item in the inventory.";
+						}
 					}
 				}
-			}
-			unset($_SESSION['items']);
+				unset($_SESSION['items']);
 
-		?>
-		<form action="../controller/search.php" method="post" >
-			<fieldset>
-				<legend>Search items</legend>
+			?>
+			<form action="../controller/search.php" method="post" >
+				<fieldset>
+					<legend>Search Items</legend>
+						<p>
+							<label for="itemName">Item Name</label>
+							<input type="text" name="itemName" id="itemName" maxlength="40" size="40" required="required" pattern="^[A-Za-z ]{1,40}$"/>
+						</p>
+						<input id='page' name='page' type='hidden' value='addsales' />
+						<p>
+							<button id="searchbtn" class="btn" type="submit" name="submit" value="searchitems">Search</button>				
+						</p>
+						<p>
+							<?php
+								if (isset($_GET['com'])) {
+									if ($_GET['com'] == 0) {
+										echo "<span class='errortxt'>Failed to search the item</span>";
+									}
+								}
+							?>
+
+						</p>
+				</fieldset>
+			</form>
+
+
+			<!-- add php to process this form to db -->
+			<form action="../controller/addsales.php" method="post">
+				<fieldset>
+					<legend>Add Sales Record</legend>
 					<p>
 						<label for="itemName">Item Name</label>
 						<input type="text" name="itemName" id="itemName" maxlength="40" size="40" required="required" pattern="^[A-Za-z ]{1,40}$"/>
 					</p>
-					<input id='page' name='page' type='hidden' value='addsales' />
 					<p>
-	    				<button id="searchbtn" class="btn" type="submit" name="submit" value="searchitems">Search</button>				
+						<label for='qty'>Quantity</label>
+						<input id='qty' name='qty' type='text' size='10' required='required' pattern="^[0-9]+$"/>
 					</p>
 					<p>
+						<label for='salesDate'>Date Sold</label>
 						<?php
-							if (isset($_GET['com'])) {
-								if ($_GET['com'] == 0) {
-									echo "<span class='errortxt'>Failed to search the item</span>";
-								}
-							}
+							date_default_timezone_set("Australia/Melbourne");
+							echo "<input id='salesDate' name='salesDate' type='datetime' size='40' readonly value='". date("Y-m-d H:i:s") ."'/>"
 						?>
 
 					</p>
-			</fieldset>
-		</form>
+					<p>
+						<span class="errortxt" id="statetxt"></span>
+					</p>
+					<p>
+					<?php 
+					// Return error messages
+					if (isset($_GET['suc'])) {
+						if ($_GET['suc'] == 's1') {
+							echo "<span class='suctxt'>Successfully added!</span>";
+						}
+					} else if (isset($_GET["err"])) {
+						$error = $_GET["err"];
 
-
-		<!-- add php to process this form to db -->
-		<form action="../controller/addsales.php" method="post">
-			<fieldset>
-				<legend>Add Sales Record</legend>
-				<p>
-					<label for="itemName">Item Name</label>
-					<input type="text" name="itemName" id="itemName" maxlength="40" size="40" required="required" pattern="^[A-Za-z ]{1,40}$"/>
-				</p>
-				<p>
-					<label for='qty'>Quantity</label>
-					<input id='qty' name='qty' type='text' required='required' pattern="^[0-9]+$"/>
-				</p>
-				<p>
-					<label for='salesDate'>Date Sold</label>
-					<?php
-						date_default_timezone_set("Australia/Melbourne");
-						echo "<input id='salesDate' name='salesDate' type='datetime' readonly value='". date("Y-m-d H:i:s") ."'/>"
+						if ($error == "s1") {
+							echo "<span class='errortxt'>Can not create a sales table</span>";
+						} else if ($error == "s2") {
+							echo "<span class='errortxt'>There is no item with the given name</span>";
+						} else if ($error == "s3") {
+							echo "<span class='errortxt'>Cannot update inventory table</span>";
+						} else if ($error == "s4") {
+							echo "<span class='errortxt'>Cannot insert into sales table</span>";
+						}
+					} else if (isset($_GET["msg"])) {
+						if ($_GET["msg"] == "s1") {
+							echo "<span class='errortxt'>Not much item in stock</span>";
+						}
+					}
 					?>
-
-				</p>
-				<p>
-					<span class="errortxt" id="statetxt"></span>
-				</p>
-				<p>
-				<?php 
-				// Return error messages
-				if (isset($_GET['suc'])) {
-					if ($_GET['suc'] == 's1') {
-						echo "<span class='suctxt'>Successfully added!</span>";
-					}
-				} else if (isset($_GET["err"])) {
-					$error = $_GET["err"];
-
-					if ($error == "s1") {
-						echo "<span class='errortxt'>Can not create a sales table</span>";
-					} else if ($error == "s2") {
-						echo "<span class='errortxt'>There is no item with the given name</span>";
-					} else if ($error == "s3") {
-						echo "<span class='errortxt'>Cannot update inventory table</span>";
-					} else if ($error == "s4") {
-						echo "<span class='errortxt'>Cannot insert into sales table</span>";
-					}
-				} else if (isset($_GET["msg"])) {
-					if ($_GET["msg"] == "s1") {
-						echo "<span class='errortxt'>Not much item in stock</span>";
-					}
-				}
-				?>
-				</p>
-				<p>
-	    			<button id="addbtn" class="btn" type="submit" name="submit" value="addsales">Add</button>				
-					<input type="reset" value="Reset Form"/>
-				</p>
-			</fieldset>
-		</form>
+					</p>
+					<p>
+						<button id="addbtn" class="btn" type="submit" name="submit" value="addsales">Submit</button>				
+						<input type="reset" value="Reset"/>
+					</p>
+				</fieldset>
+			</form>
+			<?php include_once "footer.inc"; ?>
+		</div>
 	</body>
-	<?php include_once "footer.inc"; ?>
 </html>
--- apps/view/editInventory.php
+++ apps/view/editInventory.php
@@ -8,94 +8,97 @@
 		<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 	</head>
 	<body>
-	<?php include_once 'header.inc';?>
-		<h1>Display an Inventory Record</h1>
-		<table border="1">
-			<tr>
-				<th>Item Name</th>
-				<th>Item Price</td>
-				<th>Quantity in Stock</th>
-				<th>Category</th>
-			</tr>
-			
-			<?php
-				$errMsg = "";
-				session_start();
-
-				if(!isset($_SESSION['data'])) {
-					if (isset($_GET['com'])) {
-						$msg = $_GET['com'];
-						header ("location: ../controller/editinventory.php?search=1&com=$msg");
-					} else {
-						header ("location: ../controller/editinventory.php?search=1");
-					}
-				} else {
-					$data = $_SESSION['data'];
-					if (is_array($data)) {
-						foreach($data as $row) {
-							echo "<tr>";
-							echo "<td>" . $row[1] . "</td>";
-							echo "<td>" . $row[2] . "</td>";
-							echo "<td>" . $row[3] . "</td>";
-							echo "<td>" . $row[4] . "</td>";
-							echo "</tr>";
-						}
-					} else {
-						$errMsg .= $data;
-					}
-					unset($_SESSION['data']);
-				}
-			?>
-
-		 v
-		<input type="button" id="btnEdit" value="Edit" onClick="editInventory()"/>
-		<form id="editForm" method="post" action="../controller/editinventory.php" style="visibility:hidden">
+	<div class="content">
+		<?php include_once 'header.inc';?>
+		<?php include 'menu.inc';?>
 			<fieldset>
-				<legend>Inventory Record</legend>
-					<p>
-						<label for="itemName">Item Name</label>
-						<input type="text" name="itemName" id="itemName" maxlength="40" size="40" required="required" pattern="^[A-Za-z ]{1,40}$"/>
-					</p>
-					<p>
-						<label for="itemPrice">Item Price</label>
-						<input type="text" name="itemPrice" id="itemPrice" maxlength="8" size="8" pattern="([0-9])+(\.[0-9]{1,2})?" />
-					</p>
-					<p>
-						<label for="itemStock">Quantity in Stock</label>
-						<input type="text" name="itemStock" id="itemStock" maxlength="10" size="10" pattern="[0-9]+"/>
-					</p>
-					<p>
-						<label for="itemCate">Item Category</label>
-						<input type="text" name="itemCate" id="itemCate" maxlength="30" size="30" required="required" pattern="^[A-Za-z0-9]{1,30}$"/>
-					</p>
-					<p> Update quanity
-						<select name="updateOption" id="updateOption">
-							<option value="Add">Add</option>
-							<option value="Subtract">Subtract</option>
-						</select>
-					</p>
-					<p>
-					<span class="errortxt" id="statetext"></span>
-					</p>
-					<p>
-					<?php 
-						// Return error messages
+			<table>
+				<tr>
+					<th>Item Name</th>
+					<th>Item Price</td>
+					<th>Quantity in Stock</th>
+					<th>Category</th>
+				</tr>
+				
+				<?php
+					$errMsg = "";
+					session_start();
+
+					if(!isset($_SESSION['data'])) {
 						if (isset($_GET['com'])) {
 							$msg = $_GET['com'];
-							$li = array("Successfully updated item price!<br/>", "Failed to udpate item price!<br/>", "Successfully updated item stock!<br/>", "Failed to udpate item stock!<br/>", "Successfully updated item category!<br/>", "Failed to udpate item category!<br/>");
-
-							for ($i=0; $i < strlen($msg); $i++) {
-								echo $li[$msg[$i]];
-							}
+							header ("location: ../controller/editinventory.php?search=1&com=$msg");
+						} else {
+							header ("location: ../controller/editinventory.php?search=1");
 						}
-					?>
-					</p>
-					<input type="submit" value="Submit"/>
-					<input type="reset" value="Reset"/>
-					<input type="button" id="editCancel" value="Cancel" onClick="cancelEdit()"/>
-		</form>
+					} else {
+						$data = $_SESSION['data'];
+						if (is_array($data)) {
+							foreach($data as $row) {
+								echo "<tr>";
+								echo "<td>" . $row[1] . "</td>";
+								echo "<td>" . $row[2] . "</td>";
+								echo "<td>" . $row[3] . "</td>";
+								echo "<td>" . $row[4] . "</td>";
+								echo "</tr>";
+							}
+						} else {
+							$errMsg .= $data;
+						}
+						unset($_SESSION['data']);
+					}
+				?>
+			</table>
+			</fieldset>
+			
+			<form id="editForm" method="post" action="../controller/editinventory.php">
+				<fieldset>
+					<legend>Edit Inventory Record</legend>
+						<p>
+							<label for="itemName">Item Name</label>
+							<input type="text" name="itemName" id="itemName" maxlength="40" size="40" required="required" pattern="^[A-Za-z ]{1,40}$"/>
+						</p>
+						<p>
+							<label for="itemPrice">Item Price</label>
+							<input type="text" name="itemPrice" id="itemPrice" maxlength="8" size="10" pattern="([0-9])+(\.[0-9]{1,2})?" />
+						</p>
+						<p>
+							<label for="itemStock">Quantity in Stock</label>
+							<input type="text" name="itemStock" id="itemStock" maxlength="10" size="10" pattern="[0-9]+"/>
+						</p>
+						<p>
+							<label for="itemCate">Item Category</label>
+							<input type="text" name="itemCate" id="itemCate" maxlength="30" size="40" required="required" pattern="^[A-Za-z0-9]{1,30}$"/>
+						</p>
+						<p> Update quanity
+							<select name="updateOption" id="updateOption">
+								<option value="Add">Add</option>
+								<option value="Subtract">Subtract</option>
+							</select>
+						</p>
+						<p>
+						<span class="errortxt" id="statetext"></span>
+						</p>
+						<p>
+						<?php 
+							// Return error messages
+							if (isset($_GET['com'])) {
+								$msg = $_GET['com'];
+								$li = array("Successfully updated item price!<br/>", "Failed to udpate item price!<br/>", "Successfully updated item stock!<br/>", "Failed to udpate item stock!<br/>", "Successfully updated item category!<br/>", "Failed to udpate item category!<br/>");
+
+								for ($i=0; $i < strlen($msg); $i++) {
+									echo $li[$msg[$i]];
+								}
+							}
+						?>
+						</p>
+						<input type="submit" value="Submit"/>
+						<input type="reset" value="Reset"/>
+					</fieldset>
+			</form>
+			<?php include_once "footer.inc"; ?>
+		</div>
 	</body>
-	<?php include_once "footer.inc"; ?>
 </html>
 		
 	
